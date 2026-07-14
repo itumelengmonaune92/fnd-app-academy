7aec28ee7d1f686d9227889aec9fa1241b65a7ea
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-14 21:23:03 +0200

    Removed the redundant upper method from the formatted string

--- practical-tasks/ticket-booking-system.py
+++ practical-tasks/ticket-booking-system.py
@@ -3,9 +3,9 @@ Create a program that acts as a digital ticket counter.
 """
 
 # Collect user information
-name = input("Enter you name: ")
+name = input("Enter your name: ")
 artist_band = input("Which artist/band are you here to see: ")
-confirmation_message = (f"Hey {name.upper().title()}! Your tickets to see {artist_band.upper().title()} are booked successfully!")
+confirmation_message = (f"Hey {name.title()}! Your tickets to see {artist_band.title()} are booked successfully!")
 
 # Display custom ticket confirmation message
 print()
