69d5b83b5da8e45892b52c1712d40de69bac2871
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-14 21:56:56 +0200

    Operator Precedence

---
+++ exercises/operator-precedence.py
@@ -0,0 +1,17 @@
+"""
+Operator Precedence
+BODMAS
+1. Brackets: ()
+2. Order:    powers, square roots or exponents
+3. Division:
+4. Multiplication:
+5. Addition:
+6. Subraction:
+
+NB: Multiplication and Division have a higher order
+NB: Solve expression using the left to right rule
+
+"""
+
+expression = 16 - 2 ** + 2 // 5 * 2
+print(expression)
