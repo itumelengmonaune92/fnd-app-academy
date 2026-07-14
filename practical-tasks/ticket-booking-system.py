37fa0d65100737b05cfe725ad46c61255a39f2d2
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-14 21:18:32 +0200

    Concert Ticket Booker Challenge completed

---
+++ practical-tasks/ticket-booking-system.py
@@ -0,0 +1,13 @@
+"""
+Create a program that acts as a digital ticket counter.
+"""
+
+# Collect user information
+name = input("Enter you name: ")
+artist_band = input("Which artist/band are you here to see: ")
+confirmation_message = (f"Hey {name.upper().title()}! Your tickets to see {artist_band.upper().title()} are booked successfully!")
+
+# Display custom ticket confirmation message
+print()
+print(confirmation_message)
+print()
