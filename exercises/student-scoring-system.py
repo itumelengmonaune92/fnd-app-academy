ad7f02674d62cdf2882b798c69db6c885ef15ee9
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-15 01:22:18 +0200

    initialised student scoring system exercise

---
+++ exercises/student-scoring-system.py
@@ -0,0 +1,15 @@
+"""
+Compound Boolean Expression are sued to show if two or more condition are TRUE or 
+FALSE if at least one condition is FALSE
+
+Use compound boolean to write a simple program to grade students 
+with score.
+
+Grading system:
+F -> Fail       (0 -39)
+E -> Pass       (40 -49)
+C -> Good       (50 -59)
+B -> Very Good  (60 -69)
+A -> Excellent  (70 -100)
+
+"""
