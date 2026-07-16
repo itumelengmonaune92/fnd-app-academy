b448287b115e86293bf767652c70e718a0327c81
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-16 15:30:31 +0200

    improved code readbility

--- exercises/weight-converter.py
+++ exercises/weight-converter.py
@@ -14,12 +14,12 @@ Your program should be case-insensitive to the unit input.
 """
 
 weight = int(input("Enter weight: "))
-conversion_unit = input("Unit of measurement: ").upper()
+conversion_unit = input("Unit of measurement (K)g or (L)bs: ")
 
-if weight and conversion_unit == "L":
+if conversion_unit.upper() == "L":
     weight *= 0.45
     print(f"Weight in Kg: {weight:.1f}")
-elif weight and conversion_unit == "K":
+elif conversion_unit.upper() == "K":
     weight /= 0.45
     print(f"Weight in Lbs: {weight:.1f}")
 else:
