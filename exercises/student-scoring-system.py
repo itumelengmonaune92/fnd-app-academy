fe451eecd99d85a2180f8c0076eed0ef6b9a3f29
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-16 14:41:07 +0200

    Boolean short circuit evalutaion practice complete on a Student grading system

--- exercises/student-scoring-system.py
+++ exercises/student-scoring-system.py
@@ -1,5 +1,5 @@
 """
-Compound Boolean Expression are sued to show if two or more condition are TRUE or 
+Compound Boolean Expression are used to show if two or more condition are TRUE or 
 FALSE if at least one condition is FALSE
 
 Use compound boolean to write a simple program to grade students 
@@ -13,3 +13,14 @@ B -> Very Good  (60 -69)
 A -> Excellent  (70 -100)
 
 """
+# Use Boolean short-circuit routing Evaluation (inline conditional assignment)
+student_score = int(input("Enter student score: "))
+
+results =   (student_score >= 70 and student_score <=100 and f" Total score = {student_score}: A -> Excellent!")\
+        or  (student_score >=60 and student_score <=69 and f"Total score = {student_score}: B -> Very Good!")\
+        or  (student_score >=50 and student_score <=59 and f"C -> Good: Total score = {student_score}: C -> Good")\
+        or  (student_score >=40 and student_score <=49 and f"Total score = {student_score }: E -> Pass")\
+        or  (student_score >= 0 and student_score <=39 and f"Total score = {student_score}: F -> Fail") 
+
+print(results)
+            
