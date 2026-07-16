e3fcf2b704bf0dc9dcf03eb78d52057aa84face9
Itumeleng Monaune <itumeleng.mona92@gmail.com>
2026-07-16 16:34:19 +0200

    used a while loop to prompt user 3 more attempts if use enter an invalid unit of measurement

--- exercises/weight-converter.py
+++ exercises/weight-converter.py
@@ -12,15 +12,18 @@ Conversion rules:
 Your program should be case-insensitive to the unit input.
 
 """
+attempts = 0
+while attempts < 3:
+    attempts += 1
+    weight = int(input("Enter weight: "))
+    conversion_unit = input("Unit of measurement (K)g or (L)bs: ")
+   
+    if conversion_unit.upper() == "L":
+        weight *= 0.45
+        print(f"Weight in Kg: {weight:.1f}")
+    elif conversion_unit.upper() == "K":
+        weight /= 0.45
+        print(f"Weight in Lbs: {weight:.1f}")
+    else:
+        print("Invalid entry, enter a valid unit of measurement!")
 
-weight = int(input("Enter weight: "))
-conversion_unit = input("Unit of measurement (K)g or (L)bs: ")
-
-if conversion_unit.upper() == "L":
-    weight *= 0.45
-    print(f"Weight in Kg: {weight:.1f}")
-elif conversion_unit.upper() == "K":
-    weight /= 0.45
-    print(f"Weight in Lbs: {weight:.1f}")
-else:
-    print("Invalid entry, try again!")
